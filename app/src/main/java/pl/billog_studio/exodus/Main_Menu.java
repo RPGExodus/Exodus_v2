7e349f50b72c3218226b2b47990b580dae0b975e
RPGExodus <Raver98@wp.pl>
2017-05-23 01:49:42 +0200

    Test_1

---
+++ app/src/main/java/pl/billog_studio/exodus/Main_Menu.java
@@ -0,0 +1,25 @@
+package pl.billog_studio.exodus;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Main_Menu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main__menu);
+        final Button button = (Button)findViewById(R.id.button_nowa_gra); //Button od nowej gry
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(Main_Menu.this, Gra.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+
+}
