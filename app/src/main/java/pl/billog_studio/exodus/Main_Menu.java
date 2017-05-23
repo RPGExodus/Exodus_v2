f8117889bce29b6c959f2ce15199bfe786a46ecb
RPGExodus <Raver98@wp.pl>
2017-05-23 02:36:09 +0200

    Test_1

--- app/src/main/java/pl/billog_studio/exodus/Main_Menu.java
+++ app/src/main/java/pl/billog_studio/exodus/Main_Menu.java
@@ -4,14 +4,19 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 
 public class Main_Menu extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main__menu);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getActionBar().hide();
+        this.setContentView(R.layout.activity_main__menu);
         final Button button = (Button)findViewById(R.id.button_nowa_gra); //Button od nowej gry
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
